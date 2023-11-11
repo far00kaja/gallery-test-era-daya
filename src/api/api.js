eba1b68c6c4ac48555d8e0ee3b453cb4637b536d
Dicky Darmawan <dicky.darmawan@aksesciptasolusi.com>
2023-11-11 11:43:33 +0700

    add .env.example \n add configuration for import file

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+import http from '../helper/axios'
+export async function getImage() {
+  try {
+    let request //= await http.
+    return request
+  } catch (error) {
+    return error
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import {} from  './assets/main.css'
 import App from './App.vue'
 import router from './router'
 
